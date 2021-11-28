9ff6bf690f5873ed4b7f3ed4d9e17bd12815867c
Radwan-Ahmed <radwanahmedsahed07@gmail.com>
2021-11-29 03:50:40 +0600

    commit

--- sub_pages/admin_login/admin_login.php
+++ sub_pages/admin_login/admin_login.php
@@ -4,8 +4,24 @@
         <meta charset="utf-8">
         <title>Animated login form</title>
         <link rel="stylesheet" href="admin_login.css">
+        <link rel="stylesheet" href="/style/style.css">
     </head>
     <body>
+
+<nav>
+        <a href="/html/index.php"><h1>Pet House</h1></a>
+        <ul class="navigation">
+          <li><a href="/html/index.php" class="nav-link">Home</a></li>
+          <li><a href="/sub_pages/buy_pets/buy.php" class="nav-link">Buy Pets</a></li>
+          <li><a href="/sub_pages/gallery/index.php" class="nav-link">Gallery</a></li>
+          <li><a href="/sub_pages/review/review.php" class="nav-link">Review</a></li>
+          <li><a href="/sub_pages/login_and_Register/login_signup.php" class="nav-link" target="_self">Log in/Join</a></li> 
+        </ul>
+        <button class="burger-menu" id="burger-menu">
+          <ion-icon class="bars" name="menu-outline"></ion-icon>
+        </button>
+</nav>
+
         <form class="box" action="admin_login_connection.php" method="POST">
         <h1>Admin Login</h1>
         <input type="text" name="id" placeholder="User-ID" required>

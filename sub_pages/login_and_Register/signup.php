f0a6de788c04b1128a93e780a5191e83aa1ef635
Radwan-Ahmed <radwanahmedsahed07@gmail.com>
2021-12-04 05:59:34 +0600

    signup

--- sub_pages/login_and_Register/signup.php
+++ sub_pages/login_and_Register/signup.php
@@ -16,9 +16,16 @@ $email = $_POST['email'];
 $pass = $_POST['password'];
 $encrypted_pass = sha1($pass);
 $password = base64_encode($encrypted_pass);
+$query = mysqli_query($connection, "SELECT * FROM register WHERE Email= '$email'");
+if(mysqli_num_rows($query)>0){
+    echo "<script type='text/javascript'>alert('Account Already Exists! Please Try with another Email. Thank You');
+            document.location='/sub_pages/login_and_Register/login_signup.php'</script>";
+}
+else{
 
-$data = "INSERT INTO register (Name,Email,Password) VALUES ('$name','$email','$password')";
+    $data = "INSERT INTO register (Name,Email,Password) VALUES ('$name','$email','$password')";
+    mysqli_query($connection,$data);
+        header('location:signup_sucessfull.php');
+}
 
-mysqli_query($connection,$data);
-header('location:signup_sucessfull.php');
 ?>
